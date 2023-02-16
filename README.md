c2cf787187dd91e8d8d0de50be1a386affbd655e
ZZ Si <zhangzhang.si@gmail.com>
2023-02-16 02:58:54 +0000

    init

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-<package-name> is a library for ....
+`colab_data_utils` is a library for data visualization and labeling in colab. This is adapted from [tensorflow's models repo](https://github.com/tensorflow/models/blob/master/research/object_detection/utils/colab_utils.py).
 
 Example usage:
 
 ```
+import colab_data_utils as cdu
+
+# Label bounding boxes.
+gt_boxes = []
+cdu.annotate(train_images_np, box_storage_pointer=gt_boxes)
 ```
 
--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("README.md") as f:
     long_description = f.read()
 
 setup(
-    name="",
+    name="colab_data_utils",
     version="0.1",
-    description="",
-    url="",
+    description="Utilities for data visualization and labeling in colab",
+    url="https://github.com/zzsi/colab_utils",
     author="",
     author_email="",
-    license="MIT",
-    packages=[],
+    license="Apache 2.0",
+    packages=["colab_data_utils"],
     long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires=">=3.6",
